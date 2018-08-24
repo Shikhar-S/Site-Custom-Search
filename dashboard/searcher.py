2e9676ec7960b44afd77b9bc93d67e9f7a3a1fab
shikhar.b <shikhar.b@mmmdvshikharb.internal.directi.com>
2018-08-24 18:34:39 +0530

    added count functionality on ES

--- dashboard/searcher.py
+++ dashboard/searcher.py
@@ -1,20 +1,21 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
-from .schema import Article
 import json
 
 def search(Crawler,user_query,num_results):
-    s=Search(using=Elasticsearch())
-    q=Q("multi_match",query=user_query,fields=['title^3','keywords^2','description','content'])
+    s=Search(using=Elasticsearch(),index=('articles'),doc_type=(Crawler))
+    q=Q("multi_match",query=user_query,fields=['title^2','content'])
     s=s.query(q)
-    s=s.filter('term',crawler_name=Crawler)
+    s=s.highlight('content',fragment_size=50)
     count=s.count()
     if num_results>count:
         num_results=count
     s=s[0:num_results]
     response=s.execute()
+    return [{"title":hit.title,"description":hit.meta.highlight.content,"url":hit.url} for hit in s]
 
-
-    return [{"title":hit.title,"description":hit.description,"url":hit.url} for hit in s]
-
+def getCount(Crawler):
+    s=Search(using=Elasticsearch(),index=('articles'),doc_type=Crawler)
+    returb s.count()
+    
 
